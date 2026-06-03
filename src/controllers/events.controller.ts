b26c527cfed67635ba88cdf6408bb6a526f50fc7
Dean Stark <deanstark04@gmail.com>
2026-06-03 21:39:59 +0300

    feat: add worker pool, recovery config and event statistics

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import prisma from "../infrastructure/prisma";
+import { EventStatus } from "../generated/prisma/enums";
+
+type EventStatsResult = {
+  total: number;
+} & Record<EventStatus, number>;
 
 export async function createEvent(req: Request, res: Response) {
   const { type, payload } = req.body;
@@ -14,5 +19,36 @@ export async function createEvent(req: Request, res: Response) {
     success: true,
     message: "Event created",
     event,
+  });
+}
+
+export async function getEventsStats(req: Request, res: Response) {
+  const stats = await prisma.event.groupBy({
+    by: ["status"],
+    _count: {
+      _all: true,
+    },
+  });
+  const initialState = Object.values(EventStatus).reduce<EventStatsResult>(
+    (acc, status) => {
+      const current = { ...acc };
+      current[status] = 0;
+      return current;
+    },
+    {
+      total: 0,
+    } as EventStatsResult,
+  );
+  const results = stats.reduce<EventStatsResult>((acc, { status, _count: { _all } }) => {
+    const current = { ...acc };
+    if (!current[status]) {
+      current[status] = _all;
+      current.total += _all;
+    }
+    return current;
+  }, initialState);
+  res.status(200).json({
+    success: true,
+    results,
   });
 }
--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createEvent } from "../controllers/events.controller";
+import { createEvent, getEventsStats } from "../controllers/events.controller";
 import validationMiddleware from "../middlewares/validation.middleware";
 import { createEventSchema } from "../validations/event.validation";
 
 const router = Router();
 
 router.post("/", validationMiddleware(createEventSchema, "body"), createEvent);
+router.get("/stats", getEventsStats);
 
 export default router;
